5aef279799c4229d8ae131d33241a0e72a1370fb
Tachi <tachi@mimiga.net>
2020-12-16 12:10:36 -0800

    Stop patching short enemy names. They shouldn't be visible anymore.

--- ff6/encode-enemy-names.rb
+++ ff6/encode-enemy-names.rb
@@ -20,23 +20,6 @@ puts ""
 puts ".p816"
 puts ""
 
-puts ".segment \"PDATAENEMYNAMES\""
-puts ""
-
-puts "ff6_enemy_names:"
-enemy_rows.each do |row|
-    short_name = row[1]
-    next if short_name.nil?
-    print ".byte "
-    (0...10).each do |char_index|
-        print ", " if char_index > 0
-        ch = char_index >= short_name.length ? ' ' : short_name[char_index]
-        print "$#{table[ch.ord].to_s(16)}"
-    end
-    puts ""
-end
-puts ""
-
 puts ".segment \"DATA\""
 puts ""
 
