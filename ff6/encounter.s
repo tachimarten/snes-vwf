94767f2062c468f92354974640392eb3f5492441
Tachi <tachi@mimiga.net>
2021-01-02 15:13:47 -0800

    Rewrite the Vanish animation to use less memory. Prevents FF6 from overwriting our DMA queue during encounters.

--- ff6/encounter.s
+++ ff6/encounter.s
@@ -33,8 +33,9 @@ ITEM_L_HAND_START_TILE = 76
 
 ; FF6 globals
 
-ff6_tiles_to_draw                = $7e0010
-ff6_encounter_enemy_ids          = $7e200d
+ff6_tiles_to_draw               = $7e0010
+ff6_encounter_enemy_ids         = $7e200d
+ff6_vanish_animation_progress   = $7e61cd
 
 .segment "BSS"
 
@@ -47,6 +48,8 @@ ff6vwf_encounter_current_item_slot: .res 1
 ff6vwf_encounter_item_type_to_draw: .res 1
 ; ID of the current skill (Rage, dance, Magitek) slot we're drawing.
 ff6vwf_encounter_current_skill_slot: .res 1
+; Bitmask of PCs that are fully Vanished (animation has completed).
+ff6vwf_encounter_vanished_pcs: .res 1
 ; The DMA ring buffer.
 ff6vwf_encounter_dma_queue: .tag ff6vwf_dma_queue
 
@@ -97,6 +100,32 @@ ff6vwf_encounter_bss_end:
 .segment "PTEXTENCOUNTERRUNDMA"
     jml _ff6vwf_encounter_run_dma           ; 4 bytes
 
+; We rewrite the Vanish animation to use less WRAM because the original one overwrites our DMA
+; queue at $7f8000.
+.segment "PTEXTENCOUNTERCOPYPREVANISHSPRITE"        ; $c13050
+    rts
+
+.segment "PTEXTENCOUNTERCHECKVANISHANIMATIONDONE"   ; $c12df8
+    jsl _ff6vwf_encounter_check_vanish_animation_done
+    nop
+
+.segment "PTEXTENCOUNTERCREATEVANISHSPRITE"         ; $c13106
+    rts
+_prepare_pc_sprite:
+    jsl _ff6vwf_encounter_prepare_pc_sprite
+    rts
+
+.segment "PTEXTENCOUNTERPREPAREPCSPRITE"        ; $c13747
+    jsl _ff6vwf_encounter_prepare_pc_sprite
+    rts
+
+; Optimize the Vanish animation to use less WRAM.
+.segment "PTEXTENCOUNTERPREPAREPCSPRITES"       ; $c1373f
+    .addr _prepare_pc_sprite    ; vanish stage 3
+    .addr _prepare_pc_sprite    ; vanish stage 2
+    .addr _prepare_pc_sprite    ; vanish stage 1
+    .addr _prepare_pc_sprite    ; vanish stage 0
+
 ; FF6 function that restores the normal BG3 font by copying it from the ROM after a dialogue-style
 ; text box in an encounter has closed. We have to patch it to reupload any text we created to VRAM.
 .segment "PTEXTENCOUNTERRESTORESMALLFONT"
@@ -145,6 +174,9 @@ COMMAND_DEFEND_START_TILE = COMMAND_FIRST_TILE + COMMAND_SLOT_DEFEND*COMMAND_TIL
 .endstruct
 
     enter .sizeof(locals), STACK_LIMIT
+
+    lda #0
+    sta f:ff6vwf_encounter_vanished_pcs
 
     ldx #.loword(ff6vwf_encounter_dma_queue)
     stx locals::outgoing_args+0
@@ -1092,6 +1124,283 @@ begin_locals
 
 .export ff6vwf_encounter_draw_blank_tile_data
 
+.proc _ff6vwf_encounter_get_vanish_animation_row
+.struct locals
+    .org 1
+    pc_index        .byte   ; uint8
+    bitmask         .byte   ; uint8
+    is_vanished     .byte   ; uint8
+.endstruct
+
+ff6_current_pc_index            = $7e002c
+
+    enter .sizeof(locals), STACK_LIMIT
+
+    lda #0
+    xba
+    lda f:ff6_current_pc_index
+    sta locals::pc_index
+    tax
+    lda f:_bits8,x
+    and f:ff6vwf_encounter_vanished_pcs
+    sta locals::is_vanished
+
+    txa
+    asli 5              ; * 32, size of PC data
+    tax
+
+    ; Logic is a bit weird here, but it seems to replicate the animation correctly...
+    lda locals::is_vanished
+    beq @no_vanish_status
+
+    lda f:ff6_vanish_animation_progress,x
+    bne @out
+    lda #$30
+    bra @out
+
+@no_vanish_status:
+    lda f:ff6_vanish_animation_progress,x
+    bne :+
+    lda #0
+    bra @out    
+:   sub #$30
+    neg8
+
+@out:
+    tax
+
+    leave .sizeof(locals)
+    rts
+.endproc
+
+; farcall uint16 _ff6vwf_encounter_check_vanish_animation_done(uint16 unused,
+;                                                              uint16 pc_data_offset)
+.proc _ff6vwf_encounter_check_vanish_animation_done
+.struct locals
+    .org 1
+    bitmask         .byte   ; uint8
+.endstruct
+
+ff6_pc_status_effects           = $7e2ec1
+
+FF6_STATUS_EFFECT_VANISH = $10
+
+    phx
+    phy
+
+    enter .sizeof(locals), STACK_LIMIT
+
+    tyx
+    lda f:ff6_vanish_animation_progress,x
+    bne @out
+
+    ; The original function did this.
+    lda #0
+    sta f:$7e7b6a
+
+    lda #0
+    xba
+    lda f:$7e0098   ; current PC index
+    inc
+    and #$03
+
+    tax
+    lda f:_bits8,x
+    sta locals::bitmask
+
+    txa
+    asli 5
+    tax
+    lda f:ff6_pc_status_effects,x
+    and #FF6_STATUS_EFFECT_VANISH
+    beq @no_vanish
+
+    ; We're Vanished. Turn the bit on.
+    lda locals::bitmask
+    ora f:ff6vwf_encounter_vanished_pcs
+    bra @store_vanished_pcs
+
+@no_vanish:
+    ; We're not Vanished. Turn the bit off.
+    lda locals::bitmask
+    not8
+    and f:ff6vwf_encounter_vanished_pcs
+
+@store_vanished_pcs:
+    sta f:ff6vwf_encounter_vanished_pcs
+
+@out:
+    leave .sizeof(locals)
+    ply
+    plx
+    rtl
+.endproc
+
+; Prepares a sprite in encounters, optionally applying the Vanish filter.
+;
+; During the Vanish animation, FF6 normally makes two copies of the entire PC's sprite sheet, one
+; of which is "Vanished", and the other of which is normal. This is not needed because each sprite
+; frame is copied to a staging area at $7fa000, and the Vanish filter can be applied on-the-fly at
+; that time instead. That frees up the original sprite sheet copy at $7f8000, which is where we
+; place our ring buffer.
+.proc _ff6vwf_encounter_prepare_pc_sprite
+.struct locals
+    .org 1
+    base_dest_addr      .addr       ; chardata near *
+    dest_ptr_bp0        .faraddr    ; chardata *
+    dest_ptr_bp1        .faraddr    ; chardata *
+    src_ptr_bp0         .faraddr    ; const chardata *
+    src_ptr_bp1         .faraddr    ; const chardata *
+    tile_row            .byte       ; uint8
+    first_tile_row      .byte       ; uint8
+    vanish_row          .byte       ; uint8
+.endstruct
+
+ff6_sprite_frame_offset     = $7e0036
+ff6_sprite_index            = $7e003c
+ff6_pc_sprite_dest_addrs    = $c2e41a
+ff6_pc_sprite_src_addrs     = $c2e422
+
+    enter .sizeof(locals), STACK_LIMIT
+
+    ; Initialize variables.
+    lda #0
+    sta f:ff6_sprite_frame_offset
+    sta locals::tile_row
+
+    ; Get row above which we draw "Vanished" sprites.
+    jsr _ff6vwf_encounter_get_vanish_animation_row
+    txa
+    sta locals::vanish_row
+
+    ; Fetch sprite index.
+    a16
+    lda f:ff6_sprite_index
+    and #$00ff
+    asl
+    tax
+    a8
+
+    ; Compute destination addresses.
+    a16
+    lda f:ff6_pc_sprite_dest_addrs,x
+    sta locals::base_dest_addr
+
+    ; Compute source addresses.
+    lda f:ff6_pc_sprite_src_addrs,x
+    add f:ff6_sprite_frame_offset
+    sta locals::src_ptr_bp0+0
+    add #$10
+    sta locals::src_ptr_bp1+0
+    a8
+
+    ; Write banks.
+    lda #$7f
+    sta locals::dest_ptr_bp0+2
+    sta locals::dest_ptr_bp1+2
+    sta locals::src_ptr_bp0+2
+    sta locals::src_ptr_bp1+2
+
+    ; Copy a row of two tiles. First, calculate destination address.
+@copy_tile_row:
+    a16
+    lda locals::tile_row
+    and #$00ff
+    asl
+    tax
+    asli 2
+    a8
+    sta locals::first_tile_row
+    a16
+    lda f:@bitplane_offsets,x
+    add locals::base_dest_addr
+    sta locals::dest_ptr_bp0+0
+    add #$10
+    sta locals::dest_ptr_bp1+0
+    a8
+
+    ; Copy the two tiles that make up this row.
+    ldx #2
+@copy_tile:
+
+    ; Draw the tile row by row.
+    ldy #0
+@copy_row:
+
+    ; Should we draw this row Vanished?
+    tya
+    add locals::first_tile_row
+    cmp locals::vanish_row
+    blt @vanish
+
+    ; Draw a tile row, regular path.
+    lda [locals::src_ptr_bp0],y
+    sta [locals::dest_ptr_bp0],y
+    lda [locals::src_ptr_bp1],y
+    sta [locals::dest_ptr_bp1],y
+    iny
+    lda [locals::src_ptr_bp0],y
+    sta [locals::dest_ptr_bp0],y
+    lda [locals::src_ptr_bp1],y
+    sta [locals::dest_ptr_bp1],y
+    iny
+    bra @next_row
+
+    ; Draw a tile row of a Vanished character.
+@vanish:
+    lda [locals::src_ptr_bp1],y     ; bitplane 2
+    iny
+    ora [locals::src_ptr_bp0],y     ; bitplane 1
+    ora [locals::src_ptr_bp1],y     ; bitplane 3
+    not8
+    dey
+    and [locals::src_ptr_bp0],y
+    sta [locals::dest_ptr_bp0],y
+    lda #0
+    sta [locals::dest_ptr_bp1],y
+    iny
+    sta [locals::dest_ptr_bp0],y
+    sta [locals::dest_ptr_bp1],y
+    iny
+
+@next_row:
+    cpy #$10
+    bne @copy_row
+
+    a16
+    lda locals::src_ptr_bp0
+    add #$20
+    sta locals::src_ptr_bp0
+    add #$10
+    sta locals::src_ptr_bp1
+    lda locals::dest_ptr_bp0
+    add #$20
+    sta locals::dest_ptr_bp0
+    add #$10
+    sta locals::dest_ptr_bp1
+    a8
+
+    dex
+    bne @copy_tile
+
+    inc locals::tile_row
+    lda locals::tile_row
+    cmp #4
+    beq @out
+    jmp @copy_tile_row
+
+@out:
+    leave .sizeof(locals)
+    a16
+    lda #0
+    ldx #0
+    ldy #0
+    a8
+    rtl
+
+@bitplane_offsets:  .word $0000, $0200, $0040, $0240
+.endproc
+
 ; Patch to the encounter DMA routine.
 .proc _ff6vwf_encounter_run_dma
     ; Code that we overwrote.
@@ -1109,3 +1418,8 @@ begin_locals
     ; Tear down.
     jml $c10be1
 .endproc
+
+_bits8:
+.repeat 8, i
+    .byte 1 << i
+.endrepeat
